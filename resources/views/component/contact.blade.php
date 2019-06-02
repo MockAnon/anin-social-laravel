81efed285310814bb61baea9aaed461698d105df
Jaron Evans <jarondevans@gmail.com>
2019-06-01 22:31:51 -0400

    removed strap

--- resources/views/component/contact.blade.php
+++ resources/views/component/contact.blade.php
@@ -30,20 +30,6 @@
             <button type="submit" class="btn btn-primary"> Submit </button>
         </form>
     </div>
-    <!-- <div class="col-md-6">
-        <h3> Sign In </h3>
-        <form action="#" method="post"> 
-            <div class="form-group">
-                <label for="email"> Your E-Mail </label>
-                <label class="form-control" type="text" name="email" id="email"> Your Text </label>
-            </div>
-            <div class="form-group">
-                <label for="password"> Your Password </label>
-                <label class="form-control" type="password" name="password" id="password"> Your Text </label>
-            </div>
-            <button type="submit" class="btn btn-primary"> Submit </button>
-        </form>
-    </div> -->
  </div>
 
  <?php
--- resources/views/component/message.blade.php
+++ resources/views/component/message.blade.php
@@ -1,5 +1,5 @@
 <!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
+<button id="modalBtn">Open Modal</button>
 
 
 
@@ -10,30 +10,25 @@
 
   <!-- Modal content -->
   <div class="modal-content">
-    <span class="close">&times;</span>
+    <div class="flex-row-reverse">
+      <span class="close">&times;</span>
+    </div>
     @include('component.contact')
-    @include('component.credits')
   </div>
 
 </div>
 
 <script>
-  /* The Modal (background) */
-  // Get the modal
   var modal = document.getElementById("myModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+  var btn = document.getElementById("modalBtn");
 
-  // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on the button, open the modal 
   btn.onclick = function() {
     modal.style.display = "block";
   }
 
-  // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
   }
