d84c21476d6a92320b2351dbc768dbfeb9f69efa
Jaron Evans <jarondevans@gmail.com>
2019-06-15 17:52:45 -0400

    added film credits and video

--- resources/views/pages/generalist.blade.php
+++ resources/views/pages/generalist.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.master')
 
 @section('title')
- Development
+ Design/ Generalist
 @endsection
 
 @section('content')
 <div class="row">
     <div class="col">
-        <h3> Designer/ Generalist </h3>
-        
+        <h3> Designer, Compositor, and CG Generalist </h3>
+        <p>
+        My names Jaron D. Evans, I'm currently working as Front-End Developer and UI Designer. <br>
+        I'm a graduate of both Seneca's Visual Effects for Film and Television program, as well as Sheridan's famous BAA Animation program. Over my carrier I have worked as a designer, ui designer, animator, cg generalist, and compositor accumillating over 20 film credits, attributing to multple apps, websites, and even winning a Canadian Screen Award. I've worked on projects such as: UFO Declassifed, Texas Rising, and Beast Farmer. I'm proficent with software such as: Nuke, Modo, Fusion, Maya, Mudbox, Z Brush, After Effects, Premier, Photoshop, Boujou, and Pftrack. Please feel free to send me a message regarding any potential employment opportunities. 
+        <br> Thank you for both your time,
+        <br> and consideration.
+        </p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/6OKm4HMqyHY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
     
