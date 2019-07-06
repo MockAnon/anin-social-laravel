7dd09566b21ef4f3ba73ea6435e885da05ba656f
Jaron Evans <jarondevans@gmail.com>
2019-07-05 22:51:54 -0400

    updated website

--- resources/views/pages/development.blade.php
+++ resources/views/pages/development.blade.php
@@ -8,7 +8,12 @@
 <div class="row">
     <div class="col">
         <h3> Development </h3>
-        <p> With over a year of experience as a developer and roughly five years of Compositing experience. Experience with languages such as Javascript, Jquery, PHP, Python, CSS, SCSS and HTML. I also have experience with Frameworks such as Laravel, React, Angular, Ionic, and React Native. </p>
+        <p> With over a year of experience as a developer and roughly five years of Compositing experience. Experience with languages such as Javascript, PHP, Python, CSS, Sass and HTML. I also have experience with Frameworks such as Laravel, Angular, Ionic, and React Native. I have experience with lybraries such as jQuery and React.
+         DATABASES, SYSTEMS, CMS: MongoDB, SQL, Git, Firebase, Google FireStore </p>
+
+        <p> Resume: https://resume.creddle.io/resume/4h5yjn4rlty </p>
+        <p> LinkedIn: https://www.linkedin.com/in/jarondevans/ </p>
+        <p> GitHub: https://github.com/mockanon </p>
         
     </div>
 </div>
@@ -19,30 +24,30 @@
   <div class="card" onclick="loadDevObject('0')">
     <img src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Cat_plotting_something_evil%21.jpg">
     <div class="card__head">
-      Plotting Cat
+      BeauteVite
     </div>
     <p class="card__copy">This is the text below</p>
   </div>
   <div class="card" onclick="loadDevObject('1')">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/False_alarm_-a.jpg/1280px-False_alarm_-a.jpg">
     <div class="card__head">
-      Angry Cat
+      MoneyMate
     </div>
     <p class="card__copy">This is the text below</p>
   </div>
 
-  <div class="card" onclick="loadDevObject('2')">
+  <!-- <div class="card" onclick="loadDevObject('2')">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Neugierige-Katze.JPG/1280px-Neugierige-Katze.JPG">
     <div class="card__head">
-      Curious Cat
+      FIIN INC.
     </div>
     <p class="card__copy">This is the text below</p>
-  </div>
+  </div> -->
 
   <div class="card" onclick="loadDevObject('3')">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Al_acecho_%289272124788%29.jpg/1280px-Al_acecho_%289272124788%29.jpg">
     <div class="card__head">
-      Prowling Cat
+      Furrever
     </div>
     <p class="card__copy">This is the text below</p>
   </div>
@@ -50,7 +55,7 @@
   <div class="card" onclick="loadDevObject('4')">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Mimi%26Tigsi.jpg/1280px-Mimi%26Tigsi.jpg">
     <div class="card__head">
-      Sleepy Cat
+      Chatty App
     </div>
     <p class="card__copy">This is the text below</p>
   </div>
@@ -58,7 +63,7 @@
   <div class="card" onclick="loadDevObject('5')">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Mimi%26Tigsi.jpg/1280px-Mimi%26Tigsi.jpg">
     <div class="card__head">
-      Sleepy Cat
+      Scoodle
     </div>
     <p class="card__copy">This is the text below</p>
   </div>
@@ -85,27 +90,27 @@
 
 let devObject = [{
     img: "img/designCredits/credit1.jpg",
-    title: "Colossal",
+    title: "BeautyVite",
     description: "goes here"
   },
   { img: "img/designCredits/credit2.jpg",
-    title: "Colossal",
+    title: "MoneyMate",
+    description: "goes here"
+  },
+//   { img: "img/designCredits/credit2.jpg",
+//     title: "FIIN INC",
+//     description: "Blah Due Doe here"
+//   },
+  { img: "img/designCredits/credit2.jpg",
+    title: "Furrever",
     description: "goes here"
   },
   { img: "img/designCredits/credit2.jpg",
-    title: "Bob",
-    description: "Blah Due Doe here"
-  },
-  { img: "img/designCredits/credit2.jpg",
-    title: "Stage",
-    description: "goes here"
-  },
-  { img: "img/designCredits/credit2.jpg",
-    title: "Cheetos",
+    title: "Chatty App",
     description: "Work I did "
   },
   { img: "img/designCredits/credit2.jpg",
-    title: "Not Cheetos",
+    title: "Scoodle",
     description: "Blah Blah Blah"
   },
 ]
