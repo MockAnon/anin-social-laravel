bc185f6e654f1e9a7a1de62b1a202be0847b03c6
Jaron Evans <jarondevans@gmail.com>
2019-08-29 18:13:50 -0400

    added css

--- resources/views/component/design.blade.php
+++ resources/views/component/design.blade.php
@@ -1,7 +1,7 @@
 <div style="height: 100vh;">
 
     @include('component.designAccordion')
-    <p> Design, UX Design, Character Design, CGI, Rigging </p>
+    <!-- <p> Design, UX Design, Character Design, CGI, Rigging </p> -->
     <div class="flex-row-to-col" onload="assembleDesign()"> 
         <div style="width: 50%;">
             <h1> Design </h1>
@@ -18,21 +18,45 @@
 
 <script>
 let designPortfolio = [
-  {title: Design}, 
-  {title: UX Design},
+  {
+    title: 'Design',
+    copy: 'Design'
+  }, 
+  {
+    title: 'UX Design',
+    copy: 'Design'
+    },
+  {
+    title: 'General',
+    copy: 'Design'
+    },
+  {
+    title: 'CGI',
+    copy: 'Design'},
+  {
+    title: 'Rigging',
+    copy: 'Design'
+    }
 ];
-
-function assembleDesign() {
-  alert("ASSEMBLE");
-};
-
-assembleDesign();
-
-
 
 $(document).ready(function() { 
 
-  console.log("123123123 this peee");
+  // console.log("123123123 this peee");
+  alert("Assemble");
+
+  $("#acordTitle00").text(designPortfolio[0].title);
+  $("#acordTitle01").text(designPortfolio[1].title);
+  $("#acordTitle02").text(designPortfolio[2].title);
+  $("#acordTitle03").text(designPortfolio[3].title);
+  $("#acordTitle04").text(designPortfolio[4].title);
+
+  // $("#acordCopy00").text(designPortfolio[0].copy);
+  // $("#acordCopy01").text(designPortfolio[1].copy);
+  // $("#acordCopy02").text(designPortfolio[2].copy);
+  // $("#acordCopy03").text(designPortfolio[3].copy);
+  // $("#acordCopy04").text(designPortfolio[4].copy);
+
+  
 
 });
 
