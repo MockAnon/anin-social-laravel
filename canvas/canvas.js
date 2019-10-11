2b4e9a1add0f0d5fb2a783dd7762a47ca44c2ce8
Jaron Evans <jarondevans@gmail.com>
2019-10-10 23:12:28 -0400

    canvas changes

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,152 +1,274 @@
-// import { inherits } from 'util';
+// var canvas = document.getElementById('nokey'),
+//     can_w = parseInt(canvas.getAttribute('width')),
+//     can_h = parseInt(canvas.getAttribute('height')),
 
 var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
-// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-// c.fillRect(100, 100, 100, 100);
-// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-// c.fillRect(500, 500, 100, 100);
-// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-// c.fillRect(300, 500, 100, 100);
+// console.log(typeof can_w);
 
-// console.log('CANVAS', canvas);
-
-//Line
-
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = '#fa34a3';
-// c.stroke();
-
-//Arc / Circle
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-
-//for loop
-
-// for (i = 0; i < 2000; i++) {
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
-
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, Math.PI * 2, false);
-//     c.strokeStyle = 'blue';
-//     c.stroke();
-// }
-var mouse = {
-    x: undefined,
-    y: undefined
-};
-
-let maxRadius = 20;
-let minRadius = 2;
-
-var colorArray = ['#ffaa33', '#99ffaa', '#00ff00', '#4411aa', '#qweqwe'];
-
-window.addEventListener('mousemove', function(event) {
-    console.log(event);
-    mouse.x = event.x;
-    mouse.y = event.y;
-    console.log(mouse);
-});
-
-window.addEventListener('resize', function() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    init();
-});
-
-function Circle(x, y, dx, dy, radius) {
-    this.x = x;
-    this.y = y;
-
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-    this.minRadius = minRadius;
-    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
-
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        // c.strokeStyle = 'blue';
-        // c.stroke();
-        c.fillStyle = this.color;
-        c.fill();
+var ball = {
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        r: 0,
+        alpha: 1,
+        phase: 0
+    },
+    ball_color = {
+        r: 207,
+        g: 255,
+        b: 4
+    },
+    R = 2,
+    balls = [],
+    alpha_f = 0.03,
+    alpha_phase = 0,
+    // Line
+    link_line_width = 0.8,
+    dis_limit = 260,
+    add_mouse_point = true,
+    mouse_in = false,
+    mouse_ball = {
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        r: 0,
+        type: 'mouse'
     };
 
-    this.update = function() {
-        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-            this.dx = -this.dx;
+// Random speed
+function getRandomSpeed(pos) {
+    var min = -1,
+        max = 1;
+    switch (pos) {
+        case 'top':
+            return [randomNumFrom(min, max), randomNumFrom(0.1, max)];
+            break;
+        case 'right':
+            return [randomNumFrom(min, -0.1), randomNumFrom(min, max)];
+            break;
+        case 'bottom':
+            return [randomNumFrom(min, max), randomNumFrom(min, -0.1)];
+            break;
+        case 'left':
+            return [randomNumFrom(0.1, max), randomNumFrom(min, max)];
+            break;
+        default:
+            return;
+            break;
+    }
+}
+function randomArrayItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function randomNumFrom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+console.log(randomNumFrom(0, 10));
+// Random Ball
+function getRandomBall() {
+    var pos = randomArrayItem(['top', 'right', 'bottom', 'left']);
+    switch (pos) {
+        case 'top':
+            return {
+                x: randomSidePos(can_w),
+                y: -R,
+                vx: getRandomSpeed('top')[0],
+                vy: getRandomSpeed('top')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            };
+            break;
+        case 'right':
+            return {
+                x: can_w + R,
+                y: randomSidePos(can_h),
+                vx: getRandomSpeed('right')[0],
+                vy: getRandomSpeed('right')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            };
+            break;
+        case 'bottom':
+            return {
+                x: randomSidePos(can_w),
+                y: can_h + R,
+                vx: getRandomSpeed('bottom')[0],
+                vy: getRandomSpeed('bottom')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            };
+            break;
+        case 'left':
+            return {
+                x: -R,
+                y: randomSidePos(can_h),
+                vx: getRandomSpeed('left')[0],
+                vy: getRandomSpeed('left')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10)
+            };
+            break;
+    }
+}
+function randomSidePos(length) {
+    return Math.ceil(Math.random() * length);
+}
+
+// Draw Ball
+function renderBalls() {
+    Array.prototype.forEach.call(balls, function(b) {
+        if (!b.hasOwnProperty('type')) {
+            ctx.fillStyle = 'rgba(' + ball_color.r + ',' + ball_color.g + ',' + ball_color.b + ',' + b.alpha + ')';
+            ctx.beginPath();
+            ctx.arc(b.x, b.y, R, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fill();
+        }
+    });
+}
+
+// Update balls
+function updateBalls() {
+    var new_balls = [];
+    Array.prototype.forEach.call(balls, function(b) {
+        b.x += b.vx;
+        b.y += b.vy;
+
+        if (b.x > -50 && b.x < can_w + 50 && b.y > -50 && b.y < can_h + 50) {
+            new_balls.push(b);
         }
 
-        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
+        // alpha change
+        b.phase += alpha_f;
+        b.alpha = Math.abs(Math.cos(b.phase));
+        // console.log(b.alpha);
+    });
 
-        this.x += this.dx;
-        this.y += this.dy;
+    balls = new_balls.slice(0);
+}
 
-        //interactivity
+// loop alpha
+function loopAlphaInf() {}
 
-        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-            if (this.radius < maxRadius) {
-                this.radius += 1;
+// Draw lines
+function renderLines() {
+    var fraction, alpha;
+    for (var i = 0; i < balls.length; i++) {
+        for (var j = i + 1; j < balls.length; j++) {
+            fraction = getDisOf(balls[i], balls[j]) / dis_limit;
+
+            if (fraction < 1) {
+                alpha = (1 - fraction).toString();
+
+                ctx.strokeStyle = 'rgba(150,150,150,' + alpha + ')';
+                ctx.lineWidth = link_line_width;
+
+                ctx.beginPath();
+                ctx.moveTo(balls[i].x, balls[i].y);
+                ctx.lineTo(balls[j].x, balls[j].y);
+                ctx.stroke();
+                ctx.closePath();
             }
-        } else if (this.radius > this.minRadius) {
-            this.radius -= 1;
         }
-
-        this.draw();
-    };
-}
-
-var circleArray = [];
-
-function init() {
-    circleArray = [];
-
-    for (var i = 0; i < 800; i++) {
-        // var radius = 30;
-        var radius = Math.random() * 3 + 1;
-        var x = Math.random() * (innerWidth - radius * 2) + radius;
-        var y = Math.random() * (innerHeight - radius * 2) + radius;
-        var dx = Math.random() - 0.5;
-        var dy = Math.random() - 0.5;
-
-        // var circle = new Circle(200, 200, 3, 3, 30);
-
-        circleArray.push(new Circle(x, y, dx, dy, radius));
     }
 }
 
-// console.log(circleArray);
+// calculate distance between two points
+function getDisOf(b1, b2) {
+    var delta_x = Math.abs(b1.x - b2.x),
+        delta_y = Math.abs(b1.y - b2.y);
 
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
+    return Math.sqrt(delta_x * delta_x + delta_y * delta_y);
+}
 
-    for (var i = 0; i < circleArray.length; i++) {
-        circleArray[i].update();
+// add balls if there a little balls
+function addBallIfy() {
+    if (balls.length < 20) {
+        balls.push(getRandomBall());
     }
 }
 
-init();
+// Render
+function render() {
+    ctx.clearRect(0, 0, can_w, can_h);
 
-animate();
+    renderBalls();
 
-// circle.update();
+    renderLines();
 
-// c.beginPath();
-// c.arc(x, y, radius, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
+    updateBalls();
+
+    addBallIfy();
+
+    window.requestAnimationFrame(render);
+}
+
+// Init Balls
+function initBalls(num) {
+    for (var i = 1; i <= num; i++) {
+        balls.push({
+            x: randomSidePos(can_w),
+            y: randomSidePos(can_h),
+            vx: getRandomSpeed('top')[0],
+            vy: getRandomSpeed('top')[1],
+            r: R,
+            alpha: 1,
+            phase: randomNumFrom(0, 10)
+        });
+    }
+}
+// Init Canvas
+function initCanvas() {
+    canvas.setAttribute('width', window.innerWidth);
+    canvas.setAttribute('height', window.innerHeight);
+
+    can_w = parseInt(canvas.getAttribute('width'));
+    can_h = parseInt(canvas.getAttribute('height'));
+}
+window.addEventListener('resize', function(e) {
+    console.log('Window Resize...');
+    initCanvas();
+});
+
+function goMovie() {
+    initCanvas();
+    initBalls(30);
+    window.requestAnimationFrame(render);
+}
+goMovie();
+
+// Mouse effect
+canvas.addEventListener('mouseenter', function() {
+    console.log('mouseenter');
+    mouse_in = true;
+    balls.push(mouse_ball);
+});
+canvas.addEventListener('mouseleave', function() {
+    console.log('mouseleave');
+    mouse_in = false;
+    var new_balls = [];
+    Array.prototype.forEach.call(balls, function(b) {
+        if (!b.hasOwnProperty('type')) {
+            new_balls.push(b);
+        }
+    });
+    balls = new_balls.slice(0);
+});
+canvas.addEventListener('mousemove', function(e) {
+    var e = e || window.event;
+    mouse_ball.x = e.pageX;
+    mouse_ball.y = e.pageY;
+    // console.log(mouse_ball);
+});
