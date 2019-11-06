241913d9aa7baf04ed24b1a2d9916627652cb69a
Jaron Evans <jarondevans@gmail.com>
2019-11-05 22:37:25 -0500

    added minor minor tweaks to footer script

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,4 +1,8 @@
 @section('custom-script')
+
+<p> Footer BS </p>
+
+
   <!-- <script src="jquery-3.4.0.min.js"></script> -->
   <script src="{{ mix('js/app.js') }}"></script>
   <script type="text/javascript">
