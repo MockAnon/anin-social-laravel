9c3ea412282fa6bd6c75689bdae047b50af99b53
Jaron Evans <jarondevans@gmail.com>
2019-11-17 22:46:37 -0500

    massive header update

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,20 +1,24 @@
 
 
 <nav id="navbar" role="navigation" style="position: fixed;">
-    <img style="width:21rem; height:auto;" src="img/icons/logo.png">
-  <div class="zoom" id="menuToggle">
-   
-    <input type="checkbox" />
+  <div id="navbar-child">
+    <a href="/">
+      <img style="width:21rem; height:auto;" src="img/icons/logo.png"> 
+    </a>
+    <div class="zoom" id="menuToggle">
     
-    <span></span>
-    <span></span>
-    <span></span>
-    
-    <ul id="menu">
-      <a class="zoom" href="/"><li>Home</li></a>
-      <a class="zoom" href="/development"><li>Development</li></a>
-      <a class="zoom" href="/generalist"><li>Designer / Generalist </li></a>
-    </ul>
-    
+      <input type="checkbox" />
+      
+      <span></span>
+      <span></span>
+      <span></span>
+      
+      <ul id="menu">
+        <!-- <a class="zoom" href="/"><li>Home</li></a> -->
+        <a class="zoom" href="/development"><li>Development</li></a>
+        <a class="zoom" href="/generalist"><li>Designer / Generalist </li></a>
+      </ul>
+      
+    </div>
   </div>
 </nav>
